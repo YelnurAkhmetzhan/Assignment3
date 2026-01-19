a092016b385c09b16b9d1d67215d0ea91bb4718f
Admin <251603@astanait.edu.kz>
2026-01-19 09:28:47 +0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,13 @@ public class Main {
         Course oop = new Course("Object Oriented Programming", 25, "CS101");
         Course db = new Course("Databases", 24, "CS202");
 
-        // CRUD operations
+        // CRUD
         dao.insert(oop);
         dao.insert(db);
-
         dao.readAll();
 
         dao.updateCredits("CS101", 30);
+        dao.readAll();
 
         dao.delete("CS202");
 
@@ -22,22 +22,22 @@ public class Main {
         Professor prof1 = new Professor("Dr. Smith", "Computer Science", "smith@email.com");
         Professor prof2 = new Professor("Dr. Brown", "Computer Science", "brown@email.com");
 
-        // University logic (ООП-часть, не БД)
+        // University
         University university = new University("AITU", "Astana");
 
-        university.addCourse(oop);
-        university.addProfessor(prof1);
-        university.addProfessor(prof2);
-
-        System.out.println(university);
-
-        System.out.println("Courses with >= 25 credits:");
-        System.out.println(university.filterCoursesByCredits(25));
-
-        System.out.println("Find course CS101:");
-        System.out.println(university.findCourseById("CS101"));
-
-        Person p = prof1;
-        System.out.println("Role: " + p.getRole()); // polymorphism
+//        university.addCourse(oop);
+//        university.addProfessor(prof1);
+//        university.addProfessor(prof2);
+//
+//        System.out.println(university);
+//
+//        System.out.println("Courses with >= 25 credits:");
+//        System.out.println(university.filterCoursesByCredits(25));
+//
+//        System.out.println("Find course CS101:");
+//        System.out.println(university.findCourseById("CS101"));
+//
+//        Person p = prof1;
+//        System.out.println("Role: " + p.getRole());
     }
 }
